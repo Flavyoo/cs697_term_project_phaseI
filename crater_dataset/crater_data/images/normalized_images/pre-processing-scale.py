83756e47a194ba4b5f929aa41c8276bced2582fe
Garrett Alston <garrettalston@Garretts-MacBook-Pro.local>
2018-04-11 17:39:26 -0400

    pre-processing-scale.py and pre-processing.py now work with relative paths

--- crater_dataset/crater_data/images/normalized_images/pre-processing-scale.py
+++ crater_dataset/crater_data/images/normalized_images/pre-processing-scale.py
@@ -1,23 +1,27 @@
 import os
 import cv2 as cv
 
+THIS_DIR = os.path.dirname(__file__)
+TILE3_24_CRATER = os.path.join(THIS_DIR, '../tile3_24/crater/')
+TILE3_24_NON_CRATER = os.path.join(THIS_DIR, '../tile3_24/non-crater/')
+CRATER = os.path.join(THIS_DIR, 'crater/')
+NON_CRATER = os.path.join(THIS_DIR, 'non-cratercrater/')
+
 t = []
-s = "/Users/Nikith/Desktop/crater_data/images/tile3_24/non-crater"
+s = TILE3_24_NON_CRATER
 t = os.listdir(s)
 for t1 in t:
-    img = cv.imread("/Users/Nikith/Desktop/crater_data/images/tile3_24/non-crater/"+t1)
+    img = cv.imread(TILE3_24_NON_CRATER+t1)
     img = cv.resize(img, (200, 200))
     cv.normalize(img, img, 0, 255, cv.NORM_MINMAX)
-    cv.imwrite('normalized_images/non-crater/'+t1,img)
+    cv.imwrite(NON_CRATER+t1,img)
 
 
 t = []
-s = "/Users/Nikith/Desktop/crater_data/images/tile3_24/crater"
+s = TILE3_24_CRATER
 t = os.listdir(s)
 for t1 in t:
-    img = cv.imread("/Users/Nikith/Desktop/crater_data/images/tile3_24/crater/"+t1)
+    img = cv.imread(TILE3_24_CRATER +t1)
     img = cv.resize(img, (200, 200))
     cv.normalize(img, img, 0, 255, cv.NORM_MINMAX)
-    cv.imwrite('normalized_images/crater/'+t1,img)
-    
-    
+    cv.imwrite(NON_CRATER +t1,img)
