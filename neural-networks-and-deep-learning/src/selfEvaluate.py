bfcec1c8d71d4886237692aa8b557bf89fd978ea
Flavyoo <flavioandrade1918@gmail.com>
2018-04-19 16:32:58 -0400

    edited selfEvaluate.py

--- neural-networks-and-deep-learning/src/selfEvaluate.py
+++ neural-networks-and-deep-learning/src/selfEvaluate.py
@@ -1,8 +1,11 @@
 import os
 
 def selfEvaluate(e, mb, eta, hl):
+    if mb == 1:
+        # check quality rate for 1
+        return
     # run the program
-    intitial_quality_rate = 0.0
+    prev_average_quality_rate = 0.0
     EPOCHS = e
     MB_SIZE = mb # powers
     ETA = eta
@@ -10,20 +13,31 @@ def selfEvaluate(e, mb, eta, hl):
     """os.system("python run_experiment.py " + str(EPOCHS) + " " \
               + str(MB_SIZE) + " " + str(ETA) + " " + str(HIDDEN_LAYER) + " > selfEvaluate.txt")"""
     fileInput = open("selfEvaluate.txt")
-    content = fileInput.readlines();
-    quality_rate = content[-1].split(" ")
-    try:
-        float(element)
-    except ValueError:
-        print "Not a float"
-        print "Continuing"
-    if quality_rate > intitial_quality_rate:
-        intitial_quality_rate = quality_rate
-        selfEvaluate(EPOCHS, MB_SIZE, ETA, HIDDEN_LAYER)
-    elif quality_rate < intitial_quality_rate:
+    content = fileInput.read().splitlines();
+    temp = content[10:-3]
+    temp = [l.split() for l in temp]
+    average_quality_rate = 0.0
+    total = len(temp)
+    for line in temp:
+        # quality rate line
+        if len(line) != 0:
+            qr = line[6].replace(",", '')
+        try:
+            float(qr)
+            average_quality_rate += float(qr)
+        except ValueError:
+            total -= 1
+            continue
+
+    average_quality_rate = average_quality_rate / (total * 1.0)
+    #if prev_quality_rate != 0.0:
+    # keep changing batch size until it increases to a max but then decreases
+    if average_quality_rate > prev_average_quality_rate:
+        prev_quality_rate = average_quality_rate
         MB_SIZE = MB_SIZE / 2
+        selfEvaluate(EPOCHS, MB_SIZE, ETA, HIDDEN_LAYER)
+    elif average_quality_rate < prev_average_quality_rate:
+        return MB_SIZE * 2
 
 
-
-
-selfEvaluate(1, 200, .01, 100)
+selfEvaluate(5, 200, .01, 100)
