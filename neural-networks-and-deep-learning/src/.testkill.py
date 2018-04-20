50417f94f711eab385f5606ec371561a5d12c495
Garrett Alston <Garrett.Alston001@umb.edu>
2018-04-20 17:35:35 -0400

    10 test all set

--- neural-networks-and-deep-learning/src/.testkill.py
+++ neural-networks-and-deep-learning/src/.testkill.py
@@ -2,11 +2,8 @@
 import os
 from sys import argv as args
 
-c = 0
 with open(args[1], 'r') as f:
     for line in f:
-        if c > 2:
-            pid  = line.split(' ')[0]
-            print "killing %s" % pid
-            os.system("kill %s" % pid)
-        c += 1
+        pid  = line.split(' ')[0]
+        print "killing %s" % pid
+        os.system('kill %s'  % pid)
