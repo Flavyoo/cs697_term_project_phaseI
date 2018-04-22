5d6966c4e3553c0ad1b76fdd320f0a57c3f16f8e
Garrett Alston <garrettalston@Garretts-MacBook-Pro.local>
2018-04-22 15:18:38 -0400

    Can run tests with loaded weights and biases now

--- neural-networks-and-deep-learning/src/run_experiment.py
+++ neural-networks-and-deep-learning/src/run_experiment.py
@@ -86,6 +86,7 @@ def main():
     print "  Eta     = %s" % ETA
     print "  Hid Lyr = %s" % HIDDEN_LAYER
     if PICKLE_IN:
+        print "THIS IS IS PICKLE_IN --- %s" % PICKLE_IN
         epoch = get_pickle()
         netwk = Network([SIZE*SIZE,HIDDEN_LAYER,OUTPUT_LAYER], SIZE, epoch)
     else:
