40d5929a4bb01712006d9887f5357d11bc4ca744
ebara1775 <38257609+ebara1775@users.noreply.github.com>
2018-05-01 13:02:04 -0400

    Update run_deep_expirment.py

--- neural-networks-and-deep-learning/src/run_deep_expirment.py
+++ neural-networks-and-deep-learning/src/run_deep_expirment.py
@@ -1,7 +1,25 @@
+from sys import argv as args
 from crater_deep_network import run_experiments
 
+# Default settings
+EPOCHS = 20
+MB_SIZE = 10
+ETA = .03
+
+if len(args) > 1:
+    if args[1] != '.': EPOCHS = int(args[1])
+if len(args) > 2:
+    if args[2] != '.': MB_SIZE = int(args[2])
+if len(args) > 3:
+    if args[3] != '.': ETA = float(args[3])
+
 def main():
-  run_experiments()
+  print "Making Network...."
+  print "Parameters: "
+  print "  Epochs  = %s" % EPOCHS
+  print "  MB_Size = %s" % MB_SIZE
+  print "  Eta     = %s" % ETA
+  run_experiments(EPOCHS, MB_SIZE, ETA)
 
 if __name__ == "__main__":
   main()
