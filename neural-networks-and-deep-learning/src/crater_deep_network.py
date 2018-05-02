b3a235d2f1b5ad900b78ce9fbb49b26ef115a25a
Garrett Alston <garrettalston@Garretts-MacBook-Pro.local>
2018-05-01 20:18:24 -0400

    Phase2 Runs w/ Crater Data

--- neural-networks-and-deep-learning/src/crater_deep_network.py
+++ neural-networks-and-deep-learning/src/crater_deep_network.py
@@ -11,50 +11,58 @@ import network3
 from network3 import sigmoid, tanh, ReLU, LReLU, ELU, Network
 from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
 
-EPOCHS = 10
-MB_SIZE = 10
-ETA = .03
-training_data, validation_data, test_data = network3.load_data_shared()
+import crater_loader
 
+IMAGE_SIZE = 28
+
+EPOCHS = 10
+MB_SIZE = 1
+ETA = .03
+RUNS = 1
+
+
+#training_data, validation_data, test_data = network3.load_data_shared()
+training_data, validation_data, test_data = \
+        crater_loader.load_crater_data_phaseII_wrapper("phase2-data.pkl", 28)
 
 def leakyrelu():
     for lmbda in [0.0, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0]:
-        for j in range(3):
+        for j in range(RUNS):
             print "num %s, leaky relu, with regularization %s" % (j, lmbda)
             net = Network([
-                ConvPoolLayer(image_shape=(MB_SIZE, 1, 28, 28), 
-                              filter_shape=(20, 1, 5, 5), 
-                              poolsize=(2, 2), 
+                ConvPoolLayer(image_shape=(MB_SIZE, 1, IMAGE_SIZE, IMAGE_SIZE),
+                              filter_shape=(20, 1, 5, 5),
+                              poolsize=(2, 2),
                               activation_fn=LReLU),
-                ConvPoolLayer(image_shape=(MB_SIZE, 20, 12, 12), 
-                              filter_shape=(40, 20, 5, 5), 
-                              poolsize=(2, 2), 
+                ConvPoolLayer(image_shape=(MB_SIZE, 20, 12, 12),
+                              filter_shape=(40, 20, 5, 5),
+                              poolsize=(2, 2),
                               activation_fn=LReLU),
                 FullyConnectedLayer(n_in=40*4*4, n_out=400, activation_fn=LReLU),
                 FullyConnectedLayer(n_in=400, n_out=200, activation_fn=LReLU),
                 FullyConnectedLayer(n_in=200, n_out=100, activation_fn=LReLU),
-                SoftmaxLayer(n_in=100, n_out=10)], MB_SIZE)
+                SoftmaxLayer(n_in=100, n_out=1)], MB_SIZE)
             net.SGD(training_data, EPOCHS, MB_SIZE, ETA, validation_data, test_data, lmbda=lmbda)
-            
-def ELU():
+
+def elu():
     for lmbda in [0.0, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0]:
-        for j in range(3):
+        for j in range(RUNS):
             print "num %s, leaky relu, with regularization %s" % (j, lmbda)
             net = Network([
-                ConvPoolLayer(image_shape=(MB_SIZE, 1, 28, 28), 
-                              filter_shape=(20, 1, 5, 5), 
-                              poolsize=(2, 2), 
+                ConvPoolLayer(image_shape=(MB_SIZE, 1, IMAGE_SIZE, IMAGE_SIZE),
+                              filter_shape=(20, 1, 5, 5),
+                              poolsize=(2, 2),
                               activation_fn=ELU),
-                ConvPoolLayer(image_shape=(MB_SIZE, 20, 12, 12), 
-                              filter_shape=(40, 20, 5, 5), 
-                              poolsize=(2, 2), 
+                ConvPoolLayer(image_shape=(MB_SIZE, 20, 12, 12),
+                              filter_shape=(40, 20, 5, 5),
+                              poolsize=(2, 2),
                               activation_fn=ELU),
                 FullyConnectedLayer(n_in=40*4*4, n_out=400, activation_fn=ELU),
                 FullyConnectedLayer(n_in=400, n_out=200, activation_fn=ELU),
                 FullyConnectedLayer(n_in=200, n_out=100, activation_fn=ELU),
-                SoftmaxLayer(n_in=100, n_out=10)], MB_SIZE)
+                SoftmaxLayer(n_in=100, n_out=2)], MB_SIZE)
             net.SGD(training_data, EPOCHS, MB_SIZE, ETA, validation_data, test_data, lmbda=lmbda)
 
 def run_experiments():
     leakyrelu()
-    ELU()
+    elu()
--- neural-networks-and-deep-learning/src/crater_loader.py
+++ neural-networks-and-deep-learning/src/crater_loader.py
@@ -16,6 +16,8 @@ the test and the training data. The final values are then returned.
 
 import pickle
 import numpy as np
+import theano
+import theano.tensor as T
 
 # call this to get image data and label
 # take in a string filename
@@ -32,3 +34,32 @@ def load_crater_data_wrapper(filename):
     # test data
     ted = zip(test_data_inputs, test_data[1])
     return (trd, ted)
+
+def load_crater_data_phaseII_wrapper(filename, size):
+    my_file = open(filename, 'rb')
+    training_data, validation_data, test_data = pickle.load(my_file)
+    my_file.close()
+    training_data_inputs = [np.reshape(x, (size * size, 1)) for x in training_data[0]]
+    trd = zip(training_data_inputs, training_data[1])
+
+    validation_data_inputs = [np.reshape(x, (size * size, 1)) for x in validation_data[0]]
+    vd = zip(validation_data_inputs, validation_data[1])
+
+    test_data_inputs = [np.reshape(x, (size * size, 1)) for x in test_data[0]]
+    ted = zip(test_data_inputs, test_data[1])
+
+    def shared(data):
+        """Place the data into shared variables.  This allows Theano to copy
+        the data to the GPU, if one is available.
+
+        """
+        shared_x = theano.shared(
+            np.asarray(data[0], dtype=theano.config.floatX), borrow=True)
+        shared_y = theano.shared(
+            np.asarray(data[1], dtype=theano.config.floatX), borrow=True)
+        return shared_x, T.cast(shared_y, "int32")
+    return (shared(training_data), shared(validation_data), shared(test_data))
+
+if __name__ == '__main__':
+    training_data, validation_data, test_data = load_crater_data_phaseII_wrapper("phase2-data.pkl", 28)
+    print training_data[0]
