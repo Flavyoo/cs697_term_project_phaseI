6c392551b6f1d7e3571de161c12d22c28488f907
ebara1775 <38257609+ebara1775@users.noreply.github.com>
2018-05-10 16:47:35 -0400

    Update crater_deep_network.py

--- neural-networks-and-deep-learning/src/crater_deep_network.py
+++ neural-networks-and-deep-learning/src/crater_deep_network.py
@@ -11,16 +11,12 @@ import network3
 from network3 import sigmoid, tanh, ReLU, LReLU, ELU, Network
 from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
 
-import conv
 import crater_loader
 
-INPUT_DATA = "non_rotated_28x28.pkl"
 IMAGE_SIZE = 28
-FILTER1 = 5
-FILTER2 = 15
 
 EPOCHS = 10
-MB_SIZE = 10
+MB_SIZE = 1
 ETA = .03
 RUNS = 1
 
@@ -29,72 +25,46 @@ RUNS = 1
 
 # PHASE II -- Crater Data
 training_data, validation_data, test_data = \
-   crater_loader.load_crater_data_phaseII_wrapper(INPUT_DATA, IMAGE_SIZE)
-
-
+   crater_loader.load_crater_data_phaseII_wrapper("non_rotated_28x28.pkl", 28)
 
 def leakyrelu():
-    net = None
     for lmbda in [0.0, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0]:
         for j in range(RUNS):
             print "num %s, leaky relu, with regularization %s" % (j, lmbda)
             net = Network([
                 ConvPoolLayer(image_shape=(MB_SIZE, 1, IMAGE_SIZE, IMAGE_SIZE),
-                              filter_shape=(4, 1, FILTER1, FILTER1),
+                              filter_shape=(10, 1, 5, 5),
                               poolsize=(2, 2),
                               activation_fn=LReLU),
-                ConvPoolLayer(image_shape=(MB_SIZE, 4, IMAGE_SIZE - FILTER1 + 1, IMAGE_SIZE - FILTER1 + 1),
-                              filter_shape=(10, 4, FILTER2, FILTER2),
+                ConvPoolLayer(image_shape=(MB_SIZE, 10, 12, 12),
+                              filter_shape=(20, 10, 3, 3),
                               poolsize=(2, 2),
                               activation_fn=LReLU),
-                FullyConnectedLayer(n_in=(IMAGE_SIZE - FILTER2 + 1)*IMAGE_SIZE - FILTER2 + 1, n_out=10, activation_fn=LReLU),
-                # FullyConnectedLayer(n_in=400, n_out=200, activation_fn=LReLU),
-                # FullyConnectedLayer(n_in=200, n_out=100, activation_fn=LReLU),
-                SoftmaxLayer(n_in=10, n_out=2)], MB_SIZE)
-            net.SGD(training_data, EPOCHS, MB_SIZE, ETA, validation_data, test_data, lmbda=lmbda)
-    return net
-
-def elu():
-    net = None
-    for lmbda in [0.0, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0]:
-        for j in range(RUNS):
-            print "num %s, leaky relu, with regularization %s" % (j, lmbda)
-            net = Network([
-                ConvPoolLayer(image_shape=(MB_SIZE, 1, IMAGE_SIZE, IMAGE_SIZE),
-                              filter_shape=(20, 1, 5, 5),
-                              poolsize=(2, 2),
-                              activation_fn=ELU),
-                ConvPoolLayer(image_shape=(MB_SIZE, 20, IMAGE_SIZE, 12),
-                              filter_shape=(40, 20, 5, 5),
-                              poolsize=(2, 2),
-                              activation_fn=ELU),
-                FullyConnectedLayer(n_in=40*4*4, n_out=400, activation_fn=ELU),
-                FullyConnectedLayer(n_in=400, n_out=200, activation_fn=ELU),
-                FullyConnectedLayer(n_in=200, n_out=100, activation_fn=ELU),
+                FullyConnectedLayer(n_in=20*5*5, n_out=200, activation_fn=LReLU),
+                FullyConnectedLayer(n_in=200, n_out=200, activation_fn=LReLU),
+                FullyConnectedLayer(n_in=200, n_out=100, activation_fn=LReLU),
                 SoftmaxLayer(n_in=100, n_out=2)], MB_SIZE)
             net.SGD(training_data, EPOCHS, MB_SIZE, ETA, validation_data, test_data, lmbda=lmbda)
-    return net
 
-def dbl_conv(activation_fn=ReLU):
-    for j in range(3):
-        print "Conv + Conv + FC architecture"
-        net = Network([
-            ConvPoolLayer(image_shape=(MB_SIZE, 1, 28, 28),
-                          filter_shape=(20, 1, 5, 5),
-                          poolsize=(2, 2),
-                          activation_fn=activation_fn),
-            ConvPoolLayer(image_shape=(MB_SIZE, 20, 12, 12),
-                          filter_shape=(40, 20, 5, 5),
-                          poolsize=(2, 2),
-                          activation_fn=activation_fn),
-            FullyConnectedLayer(
-                n_in=40*4*4, n_out=100, activation_fn=activation_fn),
-            SoftmaxLayer(n_in=100, n_out=10)], MB_SIZE)
-        net.SGD(training_data, 60, MB_SIZE, 0.1, validation_data, test_data)
-    return net
+def elu():
+    for lmbda in [0.0, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0]:
+        for j in range(RUNS):
+            print "num %s, leaky relu, with regularization %s" % (j, lmbda)
+            net = Network([
+                ConvPoolLayer(image_shape=(MB_SIZE, 1, IMAGE_SIZE, IMAGE_SIZE),
+                              filter_shape=(10, 1, 11, 11),
+                              poolsize=(3, 3),
+                              activation_fn=ELU),
+                ConvPoolLayer(image_shape=(MB_SIZE, 10, 30, 30),
+                              filter_shape=(20, 10, 3, 3),
+                              poolsize=(2, 2),
+                              activation_fn=ELU),
+                FullyConnectedLayer(n_in=20*14*14, n_out=1000, activation_fn=ELU),
+                FullyConnectedLayer(n_in=1000, n_out=1000, activation_fn=ELU),
+                FullyConnectedLayer(n_in=100, n_out=100, activation_fn=ELU),
+                SoftmaxLayer(n_in=100, n_out=2)], MB_SIZE)
+            net.SGD(training_data, EPOCHS, MB_SIZE, ETA, validation_data, test_data, lmbda=lmbda)
 
 def run_experiments():
-    #conv.omit_FC()
-    net = dbl_conv()
+    leakyrelu()
     # elu()
-    #print training_data[0]
