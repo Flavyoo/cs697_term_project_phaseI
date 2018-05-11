b940104ef8175137e5628d7ce3caa63a38b3b4db
Flavyoo <flavioandrade1918@gmail.com>
2018-05-11 19:27:04 -0400

    edited plotdattafitting

--- Phase2/plotdatafitting.py
+++ Phase2/plotdatafitting.py
@@ -16,10 +16,9 @@ def plotDataFit(testData, validationData, EPOCHS):
     line1, = plt.plot(epochs, testData)
     line2, = plt.plot(epochs, validationData)
     plt.legend([line1, line2], ['Test Data', 'Validation Data'])
-    plt.plot(validationData, epochs)
     plt.show()
 
 if __name__ == '__main__':
     test = list(range(15))
     vals = list(range(15, 30))
-    plotDataFit(test, vals, 15)
+    plotDataFit([.98, .91, .95, .96, .99], [.93, .98, .91, .99, .93], 5)
