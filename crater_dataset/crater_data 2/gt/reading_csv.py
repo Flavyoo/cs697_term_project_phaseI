a6ed4b7dc77fc81940199d0163d6648aae7ad7c9
Flavyoo <flavioandrade1918@gmail.com>
2018-05-11 20:18:20 -0400

    added pyramid.py

--- crater_dataset/crater_data 2/gt/reading_csv.py
+++ crater_dataset/crater_data 2/gt/reading_csv.py
@@ -4,13 +4,7 @@ import argparse
 import time
 import cv2
 import imutils
- 
- 
- 
- 
- 
- 
- 
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
 args = vars(ap.parse_args())
@@ -23,8 +17,8 @@ with open('gt_tile3_24.csv','rb') as file:
 l=1
 for i in x:
 	cv2.circle(image,(i[0],i[1]), i[2], (0,255,255), 2)
-	
-	
+
+
 cv2.imshow("Window", image)
 cv2.waitKey(0)
 cv2.imwrite('only_crater'+'.jpg',image)
