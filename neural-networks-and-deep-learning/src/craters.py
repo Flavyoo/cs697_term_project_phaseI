393c86b803ed79cc76743cab219bc74c76c6f430
Garrett Alston <garrettalston@Garretts-MacBook-Pro.local>
2018-05-12 14:30:01 -0400

    Added Crater Datastructure

--- neural-networks-and-deep-learning/src/craters.py
+++ neural-networks-and-deep-learning/src/craters.py
@@ -23,7 +23,7 @@ class CraterList(object):
 
     def found_crater(self, point, scale, s):
         x = float(point[0]) * pow(scale, -s)
-        y = float(point[0]) * pow(scale, -s)
+        y = float(point[1]) * pow(scale, -s)
         for crater in self.craters:
             if crater.contains_pt([x, y]):
                 return True
@@ -32,18 +32,23 @@ class CraterList(object):
 
 if __name__ == '__main__':
 
-    # print "Testing Crater data structure"
-    # crater = Crater([0,0], 10)
-    # p_in = [1,1]
-    # p_out = [40,40]
-    # print crater.contains_pt(p_in)
-    # print crater.contains_pt(p_out)
+    print "Testing Crater data structure"
+    crater = Crater([0,0], 8)
+    p_in = [1.3,2.9]
+    p_out = [40,40]
+    print crater.contains_pt(p_in)
+    print crater.contains_pt(p_out)
 
     print "\nTesting CraterList data structure"
     craters = CraterList()
-    craters.add([0,0], 10)
-    craters.add([0,0], 20)
+    #craters.add([0,0], 10)
+    craters.add([0,0], 8)
     print craters.found_crater(p_in, 1, 0)
     print craters.found_crater(p_out, 1, 0)
-    craters.add([0,0], 100)
-    print craters.found_crater(p_out, 1, 0)
+    #craters.add([0,0], 100)
+    scale = 2
+    for s in range(-4, 4, 1):
+        x = float(p_in[0]) * pow(scale, -s)
+        y = float(p_in[1]) * pow(scale, -s)
+        print "[%s, %s] in craters: %s" \
+                    % (x,y,craters.found_crater(p_in, scale, s))
