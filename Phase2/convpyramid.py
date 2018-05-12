5929cb8958836e93aed740f4f9c0b1b424f1f73b
Flavyoo <flavioandrade1918@gmail.com>
2018-05-12 12:29:57 -0400

    control graph popup

--- Phase2/convpyramid.py
+++ Phase2/convpyramid.py
@@ -3,18 +3,34 @@ import time
 import cv2
 import imutils
 import sys
+from skimage.transform import pyramid_gaussian
+import argparse
+import cv2
 
 class Pyramid:
     def __init__(self, image_file_path):
         self.pyramid_image = cv2.imread(image_file_path)
         self.image_shape = self.pyramid_image.shape
+        # number of rows
         self.width = self.image_shape[0]
+        # number of columns
         self.height = self.image_shape[1]
         self.min_width = 15
         self.min_height = 15
+        self.step_size = 10;
 
     def createSlidingWindow():
-        pass
+        """
+        Create an n x n window that moves across the big image creating smaller
+        images to be passed to the convolutional neural network. Sliding windows
+        start off small and grow looking for craters that may fit inside the current
+        window.
+        """
+        for y in xrange(0, self.height, self.step_size):
+            for x in xrange(0, self.width, self.step_size):
+                yield()
+
+
 
     def runSlidingWindow():
         pass
--- Phase2/plotdatafitting.py
+++ Phase2/plotdatafitting.py
@@ -2,11 +2,15 @@
 Flavio Andrade 5-11-18
 This programs plots the test and validation accuracies.
 """
-import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
 
-def plotDataFit(testData, validationData, EPOCHS):
+def plotDataFit(testData, validationData, EPOCHS, save, file_name="accuracies_graph"):
+    if save == 1:
+        mpl.use('Agg')
+
+    import matplotlib.pyplot as plt
+    
     mpl.style.use('seaborn')
     plt.title("Test vs Validation Accuracy", color='C0')
     plt.axis([0, EPOCHS, 0, 1])
@@ -16,12 +20,14 @@ def plotDataFit(testData, validationData, EPOCHS):
     line1, = plt.plot(epochs, testData)
     line2, = plt.plot(epochs, validationData)
     plt.legend([line1, line2], ['Test Data', 'Validation Data'])
-    plt.show()
+    if save == 1:
+        plt.savefig(file_name)
+    else:
+        plt.show()
 
 if __name__ == "__main__":
     test = list(range(15))
     vals = list(range(15, 30))
     #test_numpy = np.array(test)
     #vals_numpy = np.array(vals)
-    plotDataFit([.98, .91, .95, .96, .99], [.93, .98, .91, .99, .93], 5)
-    #plotDataFit(test_numpy, vals_numpy, 15)
+    plotDataFit([.98, .91, .95, .96, .99], [.93, .98, .91, .99, .93], 5, 1, "myfile")
